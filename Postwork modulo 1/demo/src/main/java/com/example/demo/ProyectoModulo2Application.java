f0fb3e683ca1093b9e45a394503bb17ed0e59935
parker796 <katch796@hotmail.com>
2022-12-08 09:56:03 -0600

    agregamos el postwork del modulo 1 gdx en maven

---
+++ Postwork modulo 1/demo/src/main/java/com/example/demo/ProyectoModulo2Application.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoModulo2Application {
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoModulo2Application.class, args);
+	}
+}
